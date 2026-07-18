5f11486c5817b353ae08e456ae77fb929edf17f5
Subhabrata Datta <subhabrata.datta2020@gmail.com>
2026-07-18 22:13:50 +0530

    Commit003: First Python code

---
+++ python/001-firscode_python.py
@@ -0,0 +1,11 @@
+#My first Python Programming after Restarting Python Without AI 
+
+name=input("What's your name?\n: ")
+platform=input("Where are you running the code - machine/platform/IDE?\n: ")
+country=input("Which place you are from?\n: ")
+
+output_msg = "\n\n\nHi " + name + " from " + country + "!!!\n\nYou are running Datta's first ever Python Code on\n" + platform
+print(output_msg)
+input()
+print("\nconcepts used = 1.input 2.#comment 3.linebreak-n 4.concantenation 5.print\n\nBYE...\n\n")
+input()
